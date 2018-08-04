b706bbb6a642093bb69f3795d60e52e61dd9414b
ENTERPRISE\tejo8003 <josuesalvador.tejocote.consultant@nielsen.com>
2018-08-04 09:43:10 -0500

    JSTL - cambios de nombre de componente

--- cursoCetech/src/app/view/dashboard/dashboard.routing.module.ts
+++ cursoCetech/src/app/view/dashboard/dashboard.routing.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { HotelesDasboardComponent } from './hoteles-dashboard/hoteles-dashboard.component';
 
-const route: Routes=[{
-  path:'dashboard_hoteles',
-  component:HotelesDasboardComponent
+const route: Routes = [{
+  path: 'dashboard_hoteles',
+  component: HotelesDasboardComponent
 }
 ];
 
-export const DashboardRoutingModule =RouterModule.forChild(route);
+export const DashboardRoutingModule = RouterModule.forChild(route);
