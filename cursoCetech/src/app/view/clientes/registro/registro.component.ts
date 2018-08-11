c80352be4cb401ebc7632cbfb47916038c322460
ENTERPRISE\tejo8003 <josuesalvador.tejocote.consultant@nielsen.com>
2018-08-11 13:16:55 -0500

    JSTL - formularios navegacion y ruteo

--- cursoCetech/src/app/view/clientes/registro/registro.component.ts
+++ cursoCetech/src/app/view/clientes/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { datosDeHotel } from '../../dashboard/hoteles-dashboard/hoteles-dashboard.component';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
+  nombre: string;
+  correo: string;
 
-  constructor() { }
+  constructor(private params: ActivatedRoute) { }
 
   ngOnInit() {
+    if (this.params.snapshot.params['names'] !== undefined && this.params.snapshot.params['names'] !== null
+  &&  this.params.snapshot.params['correo']!== undefined && this.params.snapshot.params['correo'] !== null){
+      this.nombre = this.params.snapshot.params['names'];
+      this.correo = this.params.snapshot.params['correo'];
+    }
+    else {
+      this.nombre = datosDeHotel.nombre;
+      this.correo = datosDeHotel.correo;
+    }
     console.log('Entre a registro');
   }
 
--- cursoCetech/src/app/view/dashboard/hoteles-dashboard/hoteles-dashboard.component.ts
+++ cursoCetech/src/app/view/dashboard/hoteles-dashboard/hoteles-dashboard.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import { Component, OnInit, Input } from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '../../../../../node_modules/@angular/forms';
+
+export const datosDeHotel = {
+  nombre: '',
+  correo: ''
+};
 
 @Component({
   selector: 'app-hoteles-dashboard',
@@ -8,16 +14,62 @@ import {Router} from '@angular/router';
 })
 export class HotelesDasboardComponent implements OnInit {
 
-  constructor(private navegator:Router) { }
+  @Input('nombreDeHotel')
+  nombre: string;
+
+  registroHotelFormGroup: FormGroup;
+  constructor(private navegator:Router, private activeRoute: ActivatedRoute,
+  private formBuild: FormBuilder) { }
+
 
   ngOnInit() {
+    
+    this.registroHotelFormGroup = this.formBuild.group({
+      hotelsNameForm: ['Default',[ Validators.maxLength(8), Validators.minLength(3)] ],
+      phoneNumberForm: ['', Validators.required],
+      mailForm: [null, Validators.email]
+    });
+
+    this.registroHotelFormGroup.controls.mailForm.setValue('josue@gmail.com');
   } 
 
   /* Accion de navegacion*/
   btnAction()
   {
     console.log('Estoy en hoteles component y voy a navegar!');
-    this.navegator.navigate(['clientesRegistro']);
+    let nombre = this.registroHotelFormGroup.controls.hotelsNameForm;
+    let correo = this.registroHotelFormGroup.controls.mailForm;
+
+    if (nombre.valid && correo.valid){
+      this.navegator.navigate(['hola_dashboard/'+nombre.value+'/'+correo.value]);
+    }else{
+      this.registroHotelFormGroup.controls.hotelsNameForm.setValue('');
+      this.registroHotelFormGroup.controls.mailForm.setValue('');
+    }
+    //this.navegator.navigate(['hola_dashboard',nombre,correo]);
   }
 
+  btnActionSinParamsInURL(){
+     datosDeHotel.nombre = this.registroHotelFormGroup.controls.hotelsNameForm.value;
+     datosDeHotel.correo = this.registroHotelFormGroup.controls.mailForm.value;
+
+    this.navegator.navigate(['sindatosinurl']);
+  }
+
+
+  validarCampos(){
+    
+    let nombre = this.registroHotelFormGroup.controls.hotelsNameForm;
+    let correo = this.registroHotelFormGroup.controls.mailForm;
+    console.log('Valor de Nombre', nombre);
+    console.log('Valor de Correo', correo);
+    if (nombre.value !== '' && correo.value !== ''){
+      console.log('Campos con info');
+    }
+    if (nombre.valid && correo.valid){
+      console.log('Campos validaos');
+    } else {
+      console.log('Un campos no es valido ');
+    }
+  }
 }
